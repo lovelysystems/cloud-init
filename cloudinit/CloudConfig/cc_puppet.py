ca80a8881ef5313cb2b06d98b4654e568add7cf6
Jodok Batlogg <jodok@batlogg.com>
2011-07-31 15:38:46 +0200

    make it possible to pass certs for puppet

--- cloudinit/CloudConfig/cc_puppet.py
+++ cloudinit/CloudConfig/cc_puppet.py
@@ -30,56 +30,65 @@ def handle(name,cfg,cloud,log,args):
     e=os.environ.copy()
     dist = cloudinit.DistAction.DistAction("/etc/cloud/dist-defs.cfg")
     dist.repo_install(['puppet'])
-    # ... and then update the puppet configuration
-    if puppet_cfg.has_key('conf'):
-        # Add all sections from the conf object to puppet.conf
-        puppet_conf_fh = open('/etc/puppet/puppet.conf', 'a')
-        for cfg_name, cfg in puppet_cfg['conf'].iteritems():
-            # ca_cert configuration is a special case
-            # Dump the puppetmaster ca certificate in the correct place
-            if cfg_name == 'ca_cert':
-                # Puppet ssl sub-directory isn't created yet
-                # Create it with the proper permissions and ownership
-                if not os.path.exists('/var/lib/puppet/ssl'):
-                    os.makedirs('/var/lib/puppet/ssl')
-                    os.chmod('/var/lib/puppet/ssl', 0771)
-                    os.chown('/var/lib/puppet/ssl',
-                             pwd.getpwnam('puppet').pw_uid, 0)
-                    os.makedirs('/var/lib/puppet/ssl/certs/')
-                    os.chown('/var/lib/puppet/ssl/certs/',
-                             pwd.getpwnam('puppet').pw_uid, 0)
-                ca_fh = open('/var/lib/puppet/ssl/certs/ca.pem', 'w')
-                ca_fh.write(cfg)
-                ca_fh.close()
-                os.chown('/var/lib/puppet/ssl/certs/ca.pem',
-                         pwd.getpwnam('puppet').pw_uid, 0)
-            else:
-                puppet_conf_fh.write("\n[%s]\n" % (cfg_name))
-                for o, v in cfg.iteritems():
+    servername = puppet_cfg['conf']['puppetd']['server']
+    hostname = socket.getfqdn()
+    ssldir = '/var/lib/puppet/ssl'
+    # Puppet ssl sub-directory isn't created yet
+    # Create it with the proper permissions and ownership
+    puid = pwd.getpwnam('puppet').pw_uid
+    if not os.path.exists(ssldir):
+        os.makedirs(ssldir)
+        os.chmod(ssldir, 0771)
+        os.chown(ssldir, puid, 0)
+        os.makedirs('%s/certs' %ssldir)
+        os.chown('%s/certs' %ssldir, puid, 0)
+        os.makedirs('%s/private_keys' %ssldir)
+        os.chmod('%s/private_keys' %ssldir, 0750)
+        os.chown('%s/certs/private_keys' %ssldir, puid, 0)
+        os.makedirs('%s/public_keys' %ssldir)
+        os.chown('%s/public_keys' %ssldir, puid, 0)
+    # Add all sections from the conf object to puppet.conf
+    puppet_conf_fh = open('/etc/puppet/puppet.conf', 'a')
+    for cfg_name, cfg in puppet_cfg.iteritems():
+        fn = None
+        if cfg_name == 'conf':
+            for conf_name, conf in puppet_cfg['conf'].iteritems():
+                puppet_conf_fh.write("\n[%s]\n" % (conf_name))
+                for o, v in conf.iteritems():
                     if o == 'certname':
                         # Expand %f as the fqdn
-                        v = v.replace("%f", socket.getfqdn())
+                        v = v.replace("%f", hostname)
                         # Expand %i as the instance id
-                        v = v.replace("%i",
-                              cloud.datasource.get_instance_id())
+                        v = v.replace("%i", cloud.datasource.get_instance_id())
                         # certname needs to be downcase
                         v = v.lower()
-                    puppet_conf_fh.write("    %s = %s\n" % (o, v.lower()))
+                    puppet_conf_fh.write("    %s = %s\n" % (o, v))
                     if o == 'listen':
                         puppet_namespaceauth_fh = open('/etc/puppet/namespaceauth.conf', 'a')
                         puppet_namespaceauth_fh.write("[puppetrunner]\n")
-                        puppet_namespaceauth_fh.write("    allow %s\n" %puppet_cfg['conf']['puppetd']['server'])
+                        puppet_namespaceauth_fh.write("    allow %s\n" %servername)
                         puppet_namespaceauth_fh.close()
-        puppet_conf_fh.close()
+        # ca_cert configuration is a special case
+        # ca_cert configuration is a special case
+        # Dump the puppetmaster ca certificate in the correct place
+        elif cfg_name == 'ca_cert':
+            fn = '%s/certs/ca.pem' %ssldir
+        elif cfg_name == 'cert':
+            fn = '%s/certs/%s.pem' %(ssldir, hostname)
+        elif cfg_name == 'private_key':
+            fn = '%s/private_keys/%s.pem' %(ssldir, hostname)
+        elif cfg_name == 'public_key':
+            fn = '%s/public_keys/%s.pem' %(ssldir, hostname)
+
+        if fn is not None:
+            fh = open(fn, 'w')
+            fh.write(cfg)
+            fh.close()
+            os.chown(fn, puid, 0)
+            
+    puppet_conf_fh.close()
     # Set puppet default file to automatically start
-    # Debian/Ubuntu platforms
-    if os.path.exists('/etc/default/puppet'):
-        subprocess.check_call(['sed', '-i',
-                               '-e', 's/^START=.*/START=yes/',
-                               '/etc/default/puppet'])
-    # RedHat/CentOS platforms
-    elif os.path.exists('/sbin/chkconfig'):
-        subprocess.check_call(['chkconfig', 'puppet', 'on'])
+    subprocess.check_call(['chkconfig', 'puppet', 'on'])
     # Start puppet
     subprocess.check_call(['service', 'puppet', 'start'])
 
